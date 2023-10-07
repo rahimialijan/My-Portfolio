dcbd3b781da9cee3beb943469a93318ed488c275
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-10-07 12:01:20 +0500

    complete the navbar

--- frontend-react/src/app.js
+++ frontend-react/src/app.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Header, About, Footer, Testimonial, Skills, Work } from "./container";
+
+import { Navbar } from "./components";
+
+import "./app.scss";
 
 const app = () => {
-  return <div>app</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Footer />
+      <Testimonial />
+      <Skills />
+      <Work />
+    </div>
+  );
 };
 
 export default app;
