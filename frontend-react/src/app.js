49844d9c78c505f374f3d1e9e15a5359043637ac
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-10-22 11:20:57 +0500

    start about section

--- frontend-react/src/app.js
+++ frontend-react/src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, About, Footer, Testimonial, Skills, Work } from "./container";
+import { Header,About, Footer, Testimonial, Skills, Work } from "./container";
 
 import { Navbar } from "./components";
 
@@ -10,7 +10,7 @@ const app = () => {
     <div className="app">
       <Navbar />
       <Header />
-      <About />
+      <About/>
       <Footer />
       <Testimonial />
       <Skills />
