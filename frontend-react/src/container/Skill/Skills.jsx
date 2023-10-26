6cb8346e8bc06fd2ed465b5c12056449015e068e
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-10-26 23:28:37 +0500

    complete the skill part

--- frontend-react/src/container/Skill/Skills.jsx
+++ frontend-react/src/container/Skill/Skills.jsx
@@ -33,15 +33,15 @@ const Skills = () => {
           <motion.div className="app__skills-list">
             {skills.map((skill)=>(
               <motion.div
-                whileInView={{opacity: [0.1]}}
+                whileInView={{opacity: [0,1]}}
                 transition={{duration: 0.5}}
                 className="app__skills-item app__flex"
                 key={skill.name}
               >
                 <div className="app__flex"  style={{backgroundColor: skill.bgColor}}>
                   <img src={urlFor(skill.icon)} alt={skill.name} />
-                  <p className="p-text">{skill.name}</p>
                 </div>
+                <p className="p-text">{skill.name}</p>
               </motion.div>
             ))}
 
