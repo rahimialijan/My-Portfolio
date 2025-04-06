9026b46154910c4abd970fd8502d099b6c4c12ec
Alijan Rahimi <alijanrahimi24@yahoo.com>
2025-04-06 14:16:32 +0500

    make responsive the header section of home page

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => (
     >
       <div className="badge-cmp app__flex">
         <span>👋</span>
-        <div style={{ marginLeft: 10 }}>
+        <div style={{ marginLeft: 20 }}>
           <p className="p-text">Hello, I am</p>
           <h1 className="head-text">Ali Jan</h1>
         </div>
